c36c8fd60d05e9533a493b11bf28911a25e27e24
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2024-09-15 12:23:59 +0300

    Configure routes

--- Netflix/src/App.jsx
+++ Netflix/src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Home from "./components/home/Home";
+import Login from './components/login/Login';
 
 function App() {
     return (
         <>
-            <Home />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+            </Routes>
         </>
     )
 }
