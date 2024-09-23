09c113c1d1a0be88dff012d6ff5ae9223f58555f
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2024-09-23 19:11:10 +0300

    Add components and routes

--- Spotify/frontend/src/App.jsx
+++ Spotify/frontend/src/App.jsx
@@ -1,11 +1,15 @@
+import Display from "./components/display/Display";
+import Player from "./components/player/Player";
 import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
     return (
         <div className="h-screen bg-black">
-            <div className=".h-[90%] flex">
+            <div className="h-[90%] flex">
                 <Sidebar />
+                <Display />
             </div>
+            <Player />
         </div>
     );
 }
---
+++ Spotify/frontend/src/components/display-home/DisplayHome.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../navbar/Navbar";
+
+export default function DisplayHome() {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+}
