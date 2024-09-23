31eb7b67f953d3bb5b78a9605bc0bc5ad2dae051
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2024-09-23 18:18:49 +0300

    Initiliaze project structure

---
+++ Spotify/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./components/sidebar/Sidebar";
+
+function App() {
+    return (
+        <div className="h-screen bg-black">
+            <div className=".h-[90%] flex">
+                <Sidebar />
+            </div>
+        </div>
+    );
+}
+
+export default App;
