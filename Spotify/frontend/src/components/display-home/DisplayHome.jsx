7261d61ea90e8cbd82863f76810d878d6b9af97d
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2024-09-25 10:07:27 +0300

    Add more components

--- Spotify/frontend/src/components/display-home/DisplayHome.jsx
+++ Spotify/frontend/src/components/display-home/DisplayHome.jsx
@@ -1,9 +1,25 @@
+import { albumsData, songsData } from "../../assets/assets";
+
 import Navbar from "../navbar/Navbar";
+import AlbumItem from "../album-item/AlbumItem";
+import SongItem from "../song-item/SongItem";
 
 export default function DisplayHome() {
     return (
         <>
             <Navbar />
+            <div className="mb-4">
+                <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+                <div className="flex overflow-auto">
+                    {albumsData.map(album => <AlbumItem key={album.id} {...album} />)}
+                </div>
+            </div>
+            <div className="mb-4">
+                <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+                <div className="flex overflow-auto">
+                    {songsData.map(song => <SongItem key={song.id} {...song} />)}
+                </div>
+            </div>
         </>
     );
 }
