b5e5aeecceaf03759a1104005b25cbe16138ee2f
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2024-09-25 15:36:15 +0300

    Add song contoller functionality

--- Spotify/backend/src/controllers/songController.js
+++ Spotify/backend/src/controllers/songController.js
@@ -1,9 +1,115 @@
-const addSong = async (req, res) => {
+import { v2 as cloudinary } from "cloudinary";
 
+import songModel from "../models/songModel.js";
+
+const addSong = async (req, res) => {
+    try {
+        const name = req.body.name;
+        const desc = req.body.desc;
+        const album = req.body.album;
+        const audioFile = req.files.audio[0];
+        const imageFile = req.files.image[0];
+        const audioUpload = await cloudinary.uploader.upload(audioFile.path, { resource_type: 'video' });
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: 'image' });
+        const duration = `${Math.floor(audioUpload.duration / 60)}:${Math.floor(audioUpload.duration % 60)}`;
+
+        const songData = {
+            name,
+            desc,
+            album,
+            image: imageUpload.secure_url,
+            file: audioUpload.secure_url,
+            duration,
+        }
+
+        const song = songModel(songData);
+        await song.save();
+
+        res.json({
+            success: true,
+            message: 'Song added successfully',
+        });
+        
+    } catch (error) {
+        res.json({
+            success: false,
+            message: 'Error adding song',
+        });
+    }
 }
 
 const listSongs = async (req, res) => {
+    try {
+        const allSongs = await songModel.find({});
 
+        res.json({
+            success: true,
+            songs: allSongs,
+        });
+    } catch (error) {
+        res.json({
+            success: false,
+        });
+    }
 }
 
-export { addSong, listSongs };
+const updateSong = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, desc, album } = req.body;
+
+        let updatedData = {
+            name,
+            desc,
+            album,
+        };
+
+        if (req.files.audio) {
+            const audioFile = req.files.audio[0];
+            const audioUpload = await cloudinary.uploader.upload(audioFile.path, { resource_type: 'video' });
+            updatedData.file = audioUpload.secure_url;
+            updatedData.duration = `${Math.floor(audioUpload.duration / 60)}:${Math.floor(audioUpload.duration % 60)}`;
+        }
+
+        if (req.files.image) {
+            const imageFile = req.files.image[0];
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: 'image' });
+            updatedData.image = imageUpload.secure_url;
+        }
+
+        const updatedSong = await songModel.findByIdAndUpdate(id, updatedData, { new: true });
+
+        res.json({
+            success: true,
+            message: 'Song updated successfully',
+            song: updatedSong,
+        });
+    } catch (error) {
+        res.json({
+            success: false,
+            message: 'Error updating song',
+        });
+    }
+}
+
+const removeSong = async (req, res) => {
+    try {
+        await songModel.findByIdAndDelete(req.body.id);
+
+        res.json({
+            success: true,
+            message: 'Song removed successfully',
+        });
+    } catch (error) {
+        res.json({
+            success:false,
+        });
+    }
+}
+
+export {
+    addSong, 
+    listSongs, 
+    updateSong,
+    removeSong,
+};
--- Spotify/backend/src/routes/songRoute.js
+++ Spotify/backend/src/routes/songRoute.js
@@ -1,10 +1,13 @@
 import express from "express";
 
-import { addSong, listSongs } from "../controllers/songController.js";
+import { addSong, listSongs, updateSong, removeSong } from "../controllers/songController.js";
+import upload from "../middleware/multer.js";
 
 const songRouter = express.Router();
 
-songRouter.post('/add', addSong);
+songRouter.post('/add', upload.fields([{ name: 'image', maxCount: 1 }, {name: 'audio', maxCount: 1}]), addSong);
 songRouter.get('/songs', listSongs);
+songRouter.put('/update/:id', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'audio', maxCount: 1 }]), updateSong);
+songRouter.delete('/remove', removeSong);
 
 export default songRouter;
