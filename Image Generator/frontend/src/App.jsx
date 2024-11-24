090349d65f086e5f8423f46c8b45666d32be5941
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2024-11-24 16:23:31 +0200

    Setup routes

--- Image Generator/frontend/src/App.jsx
+++ Image Generator/frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+
+import BuyCredit from "./pages/BuyCredit";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+
 function App() {
     return (
         <>
-      
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/result' element={<Result />} />
+                <Route path='/buy' element={<BuyCredit />} />
+            </Routes>
         </>
     );
 }
