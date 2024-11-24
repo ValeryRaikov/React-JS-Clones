f18ef6ab4e52bcef58e0f0a43f39c38adc438cab
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2024-11-24 21:05:18 +0200

    Add more components and pages

--- Image Generator/frontend/src/App.jsx
+++ Image Generator/frontend/src/App.jsx
@@ -1,15 +1,21 @@
+import { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 
+import { AppContext } from "./context/AppContext";
 import BuyCredit from "./pages/BuyCredit";
 import Home from "./pages/Home";
 import Result from "./pages/Result";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
 
 function App() {
+    const { showLogin } = useContext(AppContext);
+
     return (
         <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to to-orange-50">
             <Navbar />
+            {showLogin && <Login />}
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/result' element={<Result />} />
